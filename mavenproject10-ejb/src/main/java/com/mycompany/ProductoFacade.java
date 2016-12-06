1f247255798a80fa2c4ca62c4bd819623e7d3230
Matias Vargas <matiasvm@gmail.com>
2016-12-06 20:05:46 -0300

    Commit inicial

---
+++ mavenproject10-ejb/src/main/java/com/mycompany/ProductoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import model.Producto;
+
+/**
+ *
+ * @author Rendebul
+ */
+@Stateless
+public class ProductoFacade extends AbstractFacade<Producto> implements ProductoFacadeLocal {
+
+    @PersistenceContext(unitName = "com.mycompany_mavenproject10-ejb_ejb_1.0PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductoFacade() {
+        super(Producto.class);
+    }
+    
+}
---
+++ mavenproject10-ejb/target/generated-sources/annotations/model/Producto_.java
@@ -0,0 +1,20 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-06T19:13:56")
+@StaticMetamodel(Producto.class)
+public class Producto_ { 
+
+    public static volatile SingularAttribute<Producto, String> descripcion;
+    public static volatile SingularAttribute<Producto, String> telefonoAnunciante;
+    public static volatile SingularAttribute<Producto, Integer> precio;
+    public static volatile SingularAttribute<Producto, String> nombreAnunciante;
+    public static volatile SingularAttribute<Producto, String> ciudad;
+    public static volatile SingularAttribute<Producto, Long> id;
+    public static volatile SingularAttribute<Producto, String> codigoProducto;
+    public static volatile SingularAttribute<Producto, String> nombreProducto;
+
+}
